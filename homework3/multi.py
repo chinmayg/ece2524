9edbf7ca5b9267884743200db6bb6dc19a020a49
Chinmay Ghotkar <chinmayg@vt.edu>
2012-09-17 13:38:47 -0400

    Adding multi.py

---
+++ homework3/multi.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python2
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(description='Multiplies Number from Standard Input.')
+args = parser.parse_args()
+product = 1.0
+
+try:
+	for line in iter(sys.stdin.readline,''):
+		if line == "\n":
+			print product
+			product = 1.0
+		else:
+			product = product * float(line)
+	print product
+except ValueError as e:
+    s = str(e)
+    sys.stderr.write(s)
+    sys.exit(1)
