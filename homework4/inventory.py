e7dc2ae3e577cbc1faf67537d0e81fd063a61239
Chinmay Ghotkar <chinmayg@vt.edu>
2012-10-02 16:35:42 -0400

    Added new functions to inventory.py

--- homework4/inventory.py
+++ homework4/inventory.py
@@ -4,14 +4,23 @@
 import argparse
 import sys
 import fileinput
+import shlex
 
-parser = argparse.ArgumentParser(description='This is an inventory database program.')
-parser.add_argument('-f', '--data-file', help = 'path to the data file to read at startup')
-args = parser.parse_args()
-inventory = []
+#This will read the inputs in the file
+def createInventory(fileName,inventory):
+	for line in fileinput.input(fileName):
+		splitLine = shlex.split(line)
+		tempDict = dict(PartId=splitLine[0],Description=splitLine[1],Footprint=splitLine[2],Quantity=splitLine[3])
+		inventory.append(tempDict)
+	return (inventory)
+#Main Method
+def main():
+	parser = argparse.ArgumentParser(description='This is an inventory database program.')
+	parser.add_argument('-f', '--data-file', help = 'path to the data file to read at startup')
+	args = parser.parse_args()
+	inventory = []
+	createInventory(args.data_file,inventory)
+	print len(inventory)
 
-for line in fileinput.input(args.data_file):
-	splitLine = line.split()
-	inventory.append(dict(PartId=splitLine[0],Description=splitLine[1],Footprint=splitLine[2],Quantity=splitLine[3])
-
-	x = len(inventory)
+if  __name__ =='__main__':main()
+	
