cd6f6783e55edb7a96b18d9352262a9e080ead65
Chinmay Ghotkar <chinmayg@vt.edu>
2012-10-03 15:33:26 -0400

    Working code for inventory.py and inputs

---
+++ homework4/README.md
@@ -0,0 +1,3 @@
+Features:
+I did the extra credit for the remove option.
+The remove option allows for any key value to be deleted.
--- homework4/files/runHW4.sh
+++
@@ -1,7 +0,0 @@
-rm files/ref_output files/all_output
-./inventory.py -f files/parts <files/action01 >> files/ref_output
-./inventory.py -f files/parts <files/action02 >> files/ref_output
-./inventory.py -f files/parts <files/action03 >> files/ref_output
-./inventory.py -f files/parts <files/action04 >> files/ref_output
-./inventory.py -f files/parts <files/all_actions >> files/all_output
-diff -c files/ref_output files/all_output
